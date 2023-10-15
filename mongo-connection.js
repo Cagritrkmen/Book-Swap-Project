a4d6bd7d54f1707b55594752f1329ffde912892b
Cagritrkmen <cagritrkmn33@gmail.com>
2023-10-16 00:41:24 +0300

    test

--- mongo-connection.js
+++ mongo-connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/BookSwap')
+mongoose.connect('mongodb://0.0.0.0/BookSwap')
 
 var db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
